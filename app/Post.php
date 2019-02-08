0bb92323b3a478b6f13942299c120559ba7c7593
BokiGaja <bgajic93@gmail.com>
2019-02-08 18:00:10 +0100

    first commit

---
+++ app/Post.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+//    Here we define properties that we can input (id... is automatically added)
+    protected $fillable = [
+        'title', 'body'
+    ];
+
+    public static function published()
+    {
+        return self::where('published', 1)->get();
+    }
+    public static function drafts()
+    {
+        return self::where('published', 0)->get();
+    }
+}
